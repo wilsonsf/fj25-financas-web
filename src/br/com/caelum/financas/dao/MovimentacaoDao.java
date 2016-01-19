7f6deb7132cff6c62769d833d289d8f0752c9a86
Wilson de Farias <wilsonsf1@gmail.com>
2016-01-19 13:54:13 -0200

    8.10 Adicionando notações EJB nos Daos
    	new file:   src/br/com/caelum/financas/dao/ContaDao.java
    	new file:   src/br/com/caelum/financas/dao/MovimentacaoDao.java

---
+++ src/br/com/caelum/financas/dao/MovimentacaoDao.java
@@ -0,0 +1,34 @@
+package br.com.caelum.financas.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.caelum.financas.modelo.Movimentacao;
+
+@Stateless
+public class MovimentacaoDao {
+
+	@PersistenceContext
+	EntityManager manager;
+
+	public void adiciona(Movimentacao movimentacao) {
+		this.manager.persist(movimentacao);
+	}
+
+	public Movimentacao busca(Integer id) {
+		return this.manager.find(Movimentacao.class, id);
+	}
+
+	public List<Movimentacao> lista() {
+		return this.manager.createQuery("select m from Movimentacao m", Movimentacao.class).getResultList();
+	}
+
+	public void remove(Movimentacao movimentacao) {
+		Movimentacao movimentacaoParaRemover = this.manager.find(Movimentacao.class, movimentacao.getId());
+		this.manager.remove(movimentacaoParaRemover);
+	}
+
+}
