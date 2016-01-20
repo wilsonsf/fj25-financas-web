4c55e0fe38358615a00be4a62b6ff1e9ee8d018b
Wilson de Farias <wilsonsf1@gmail.com>
2016-01-20 10:48:15 -0200

    Revert "11.9 Aplicando BMT no ContaDao para conhecer"
    Este commit reverte: aa78f02543c2be2f48d19fc21316fcf3cd3742d8.
    
    	modified:   src/br/com/caelum/financas/dao/ContaDao.java

--- src/br/com/caelum/financas/dao/ContaDao.java
+++ src/br/com/caelum/financas/dao/ContaDao.java
@@ -2,55 +2,23 @@ package br.com.caelum.financas.dao;
 
 import java.util.List;
 
-import javax.annotation.Resource;
-import javax.ejb.EJBException;
 import javax.ejb.Stateless;
-import javax.ejb.TransactionManagement;
-import javax.ejb.TransactionManagementType;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.HeuristicMixedException;
-import javax.transaction.HeuristicRollbackException;
-import javax.transaction.NotSupportedException;
-import javax.transaction.RollbackException;
-import javax.transaction.SystemException;
-import javax.transaction.UserTransaction;
 
 import br.com.caelum.financas.modelo.Conta;
 
-@TransactionManagement(TransactionManagementType.BEAN)
 @Stateless
 public class ContaDao {
-	
-	@Resource
-	UserTransaction ut;
 	
 	@PersistenceContext
 	EntityManager manager;
 
+	@TransactionAttribute(TransactionAttributeType.MANDATORY)
 	public void adiciona(Conta conta) {
-		
-		try {
-			this.ut.begin();
-		} catch (NotSupportedException | SystemException e) {
-			throw new EJBException(e);
-		}
-		
 		this.manager.persist(conta);
-		
-		try {
-			this.ut.commit();
-			
-		} catch (SecurityException | IllegalStateException | RollbackException | 
-				HeuristicMixedException	| HeuristicRollbackException | SystemException e) {
-			try {
-				this.ut.rollback();
-				
-			} catch (IllegalStateException | SecurityException | SystemException e1) {
-				throw new EJBException(e1);
-			}
-			throw new EJBException(e);
-		}
 	}
 
 	public Conta busca(Integer id) {
