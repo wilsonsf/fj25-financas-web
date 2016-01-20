749aff1599fa722b18755c725e3c7e4f22ebe873
Wilson de Farias <wilsonsf1@gmail.com>
2016-01-20 15:42:42 -0200

    13.7 Correção da classe Categoria - notação @Entity

--- src/br/com/caelum/financas/modelo/Categoria.java
+++ src/br/com/caelum/financas/modelo/Categoria.java
@@ -2,10 +2,12 @@ package br.com.caelum.financas.modelo;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
