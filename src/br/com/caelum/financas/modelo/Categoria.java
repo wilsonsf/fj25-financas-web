c88715142e88c95cd76ba1cbb08dc48ccb57cb60
Wilson de Farias <wilsonsf1@gmail.com>
2016-01-20 15:36:25 -0200

    12.7 Criada classe Categoria
    	new file:   src/br/com/caelum/financas/modelo/Categoria.java

---
+++ src/br/com/caelum/financas/modelo/Categoria.java
@@ -0,0 +1,34 @@
+package br.com.caelum.financas.modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Categoria implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@Override
+	public String toString() {
+		return this.nome;
+	}
+}
