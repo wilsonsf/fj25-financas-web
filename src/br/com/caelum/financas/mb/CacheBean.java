1e6fcf87e70e659d7a9ef0677fe6b00a5ca0f9cc
Wilson de Farias <wilsonsf1@gmail.com>
2016-01-21 14:19:45 -0200

    15.2 Cache 1º nível usando DAO/OSIV

--- src/br/com/caelum/financas/mb/CacheBean.java
+++ src/br/com/caelum/financas/mb/CacheBean.java
@@ -3,8 +3,8 @@ package br.com.caelum.financas.mb;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
 
+import br.com.caelum.financas.dao.ContaDao;
 import br.com.caelum.financas.modelo.Conta;
 
 @Named
@@ -12,14 +12,14 @@ import br.com.caelum.financas.modelo.Conta;
 public class CacheBean {
 	
 	@Inject
-	private EntityManager manager;
+	private ContaDao dao; 
 	
 	private Integer id;
 	private Conta conta;
 	
 	public void pesquisar() {
-		this.conta = this.manager.find(Conta.class, id);
-		this.conta = this.manager.find(Conta.class, id);
+		this.conta = this.dao.busca(id);
+		this.conta = this.dao.busca(id);
 	}
 
 	public Integer getId() {
