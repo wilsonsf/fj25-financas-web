8c5226f65a415c78fcbc35fdd922d3ec2f24a04e
Wilson de Farias <wilsonsf1@gmail.com>
2016-01-21 14:04:01 -0200

    15.2 Trabalhando com Cache de primeiro nível
    	modified:   src/br/com/caelum/financas/mb/CacheBean.java

--- src/br/com/caelum/financas/mb/CacheBean.java
+++ src/br/com/caelum/financas/mb/CacheBean.java
@@ -1,18 +1,25 @@
 package br.com.caelum.financas.mb;
 
-import br.com.caelum.financas.modelo.Conta;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
 
 @Named
 @RequestScoped
 public class CacheBean {
 	
+	@Inject
+	private EntityManager manager;
+	
 	private Integer id;
 	private Conta conta;
 	
 	public void pesquisar() {
-			System.out.println("Testando cache de primeiro nivel");
+		this.conta = this.manager.find(Conta.class, id);
+		this.conta = this.manager.find(Conta.class, id);
 	}
 
 	public Integer getId() {
