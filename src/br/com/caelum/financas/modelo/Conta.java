52d9c18900069265504548c8c5439d84e2550a4f
Wilson de Farias <wilsonsf1@gmail.com>
2016-01-22 11:26:55 -0200

    16.2 Adicionada validação de Conta por Constraint e valor padrão para Versão
    	modified:   src/br/com/caelum/financas/modelo/Conta.java

--- src/br/com/caelum/financas/modelo/Conta.java
+++ src/br/com/caelum/financas/modelo/Conta.java
@@ -5,18 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Cacheable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.persistence.Version;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 @Cacheable
-@Entity
+@Entity @Table(uniqueConstraints = {
+		@UniqueConstraint(columnNames= {"agencia","numero"})
+		})
 public class Conta implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -27,10 +32,12 @@ public class Conta implements Serializable {
 	private String titular;
 	private String agencia;
 	private String numero;
+	
+	@Column(length=20, nullable=false)
 	private String banco;
 	
 	@Version
-	private Integer versao;
+	private Integer versao = 0;
 
 	@Cache(usage=CacheConcurrencyStrategy.TRANSACTIONAL)
 	@OneToMany(mappedBy="conta")
